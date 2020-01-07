eb26f59b78e1c60f887a45088c42013dd0b4e143
Daniel Dick <ddick@cs.ualberta.ca>
2020-01-07 08:45:06 -0700

    more changes

--- 404Lab1.py
+++ 404Lab1.py
@@ -6,5 +6,5 @@ google = requests.get("https://www.google.com")
 #Printing the response from google (should be 200!)
 print(google)
 
-var = requests.get("https://raw.githubusercontent.com/Dan767/404Lab1/master/lab1.py")
+var = requests.get("https://raw.githubusercontent.com/Dan767/404Lab1/master/404Lab1.py")
 print(var.content)
